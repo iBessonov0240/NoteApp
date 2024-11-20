a23c7ce02ed87c8637326e4edb01642becc0b9f9
Ilia <ilya2491@gmail.com>
2024-11-20 12:22:37 +0700

    Add initial project.

---
+++ NoteAppTests/NoteAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  NoteAppTests.swift
+//  NoteAppTests
+//
+//  Created by i0240 on 20.11.2024.
+//
+
+import Testing
+@testable import NoteApp
+
+struct NoteAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
